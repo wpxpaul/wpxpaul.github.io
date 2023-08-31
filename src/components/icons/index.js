2e8364c29a9819fa0c098168828a063bb6cb3d6c
wpxpaul <wpx.paul@gmail.com>
2023-08-30 22:15:22 -0700

    first commit

---
+++ src/components/icons/index.js
@@ -0,0 +1,21 @@
+import IconSun from "./IconSun.astro"
+import IconUnlock from "./IconUnlock.astro"
+import IconPreview from "./IconPreview.astro"
+import IconHome from "./IconHome.astro"
+import IconInstagram from "./IconInstagram.astro"
+import IconUser from "./IconUser.astro"
+import IconBlog from "./IconBlog.astro"
+import IconHoopdex from "./IconHoopdex.astro"
+import IconMenu from "./IconMenu.astro"
+
+export {
+  IconSun,
+  IconUnlock,
+  IconPreview,
+  IconHome,
+  IconInstagram,
+  IconUser,
+  IconBlog,
+  IconMenu,
+  IconHoopdex
+}
